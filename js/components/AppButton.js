68563e75b61ddaed638fbb8a257c4daed62eb25f
ashrafalmayah <ashrafalmayah00@gmail.com>
2024-04-28 11:53:16 +0300

    component props

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -1,13 +1,26 @@
 export default {
     template: `
-                <button class="bg-gray-600 hover:bg-gray-800 rounded disabled:cursor-not-allowed text-white px-6 py-2" :disabled="processing" @click="buttonClick">
+                <button :class = "{
+                    'relative rounded disabled:cursor-not-allowed text-white px-6 py-2' : true,
+                    'bg-blue-600 hover:bg-blue-800' : type === 'primary',
+                    'bg-maroon-600 hover:bg-maroon-800' : type === 'secondary',
+                    'bg-gray-600 hover:bg-gray-800' : type === 'muted',
+                    'text-transparent' : processing
+                 }"
+                 :disabled="processing" @click="buttonClick">
+                    <div :class = "{'loader' : processing}"></div>
                     <slot />
                 </button>
             `,
-    data() {
-        return {
-            processing: false,
-        };
+    props: {
+        type: {
+            type: String,
+            default: "primary",
+        },
+        processing: {
+            type: Boolean,
+            default: false,
+        },
     },
     methods: {
         buttonClick() {
