a357c60a55f40bd96392a749224ff3bec60e4f2f
ashrafalmayah <ashrafalmayah00@gmail.com>
2024-04-29 14:48:50 +0300

    fix style

--- js/components/AssignmentCreate.js
+++ js/components/AssignmentCreate.js
@@ -1,8 +1,8 @@
 export default {
     template: /*html*/ `
-<form @submit.prevent="add" class="text-black bg-white rounded-lg overflow-clip divide-x-4 divide-gray-800">
-    <input v-model="newAssignment" class="p-2" placeholder="New assignment..." type="text"></input>
-    <button class="p-2">Add</button>
+<form @submit.prevent="add" class="text-black flex bg-white rounded-lg overflow-clip divide-x-4 divide-gray-800">
+    <input v-model="newAssignment" class="p-2 flex-1" placeholder="New assignment..." type="text"></input>
+    <button class="py-2 px-4">Add</button>
  </form>`,
     data() {
         return {
