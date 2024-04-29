290d1360dd04015b59be50675730f9db9b70d945
ashrafalmayah <ashrafalmayah00@gmail.com>
2024-04-29 15:04:38 +0300

    component responsibility, extract tags component

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -1,8 +1,10 @@
 import Assignment from "./Assignment.js";
+import AssignmentTags from "./AssignmentTags.js";
 
 export default {
     components: {
         Assignment,
+        AssignmentTags
     },
     template: /* html */ `
 <section v-show="filteredAssignments.length">
@@ -10,14 +12,9 @@ export default {
         {{ title }}
         <span>{{ filteredAssignments.length }}</span>
     </h2>
-    <div class="space-x-2 max-w-72 flex overflow-x-auto pb-1">
-        <button 
-            v-for="tag in tags" 
-            @click="currentTag = tag;" 
-            class="bg-gray-900 rounded-lg px-2 py-1" 
-            :class="{'!bg-blue-500' : currentTag === tag}"
-        >{{ tag }}</button>
-    </div>
+
+    <assignment-tags @change="currentTag = $event" :initial-tags="assignments.map((a) => a.tag)" :current-tag="currentTag"/>
+
     <ul class="space-y-2 max-w-72 mt-4">
         <assignment v-for="assignment in filteredAssignments" :key="assignment.id" :assignment="assignment"></assignment>
     </ul>
@@ -34,9 +31,6 @@ export default {
             }
 
             return this.assignments.filter((a) => a.tag === this.currentTag);
-        },
-        tags() {
-            return ["all", ...new Set(this.assignments.map((a) => a.tag))];
         },
     },
     props: {
---
+++ js/components/AssignmentTags.js
@@ -0,0 +1,20 @@
+export default {
+    template: `
+    <div class="space-x-2 max-w-72 flex overflow-x-auto pb-1">
+        <button 
+            v-for="tag in tags" 
+            @click="$emit('change', tag)"
+            class="bg-gray-900 rounded-lg px-2 py-1" 
+            :class="{'!bg-blue-500' : currentTag === tag}"
+        >{{ tag }}</button>
+    </div>`,
+    props: {
+        initialTags: Array,
+        currentTag: String,
+    },
+    computed: {
+        tags() {
+            return ["all", ...new Set(this.initialTags)];
+        },
+    },
+};
